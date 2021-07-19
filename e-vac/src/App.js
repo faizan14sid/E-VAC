ec17e619b691c0c705bf854566161163cf547adb
Faizan Ahmad Siddiqui <faizan14sid@gmail.com>
2021-07-19 16:07:32 +0530

    google maps integration

--- e-vac/src/App.js
+++ e-vac/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <Footer />
     </div>
   );
 }
--- e-vac/src/Components/Home.js
+++ e-vac/src/Components/Home.js
@@ -1,13 +1,59 @@
 import React from "react";
-// import { MapContainer } from "./MapContainer";
+import GoogleMap from "./GoogleMap";
+import { Card, Carousel, Button } from "react-bootstrap";
+import Footer from "./Footer";
 
 const Home = () => {
   return (
     <div>
-      <h1>
-        Home Page
-        {/* <MapContainer /> */}
-      </h1>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div
+          style={{
+            width: "70vw",
+            height: "70vh",
+          }}
+        >
+          <Carousel>
+            <Carousel.Item interval={1500}>
+              <img
+                className="d-block w-50"
+                src="https://www.fleetrobo.com/blog/wp-content/uploads/2020/05/save-maximum-lives-with-intelligent-ambulance-fleet-management-system.jpg"
+                alt="First slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item interval={1500}>
+              <img
+                className="d-block w-50"
+                src="https://adda.io/blog/wp-content/uploads/2020/04/Emergency-Contact-1.png"
+                alt="Second slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item interval={1500}>
+              <img
+                className="d-block w-50"
+                src="https://thumbs.dreamstime.com/z/all-ambulance-car-via-mobile-phone-emergency-call-ambulance-car-hands-dialing-number-ambulance-service-operator-hospital-building-119941641.jpg"
+                alt="Third slide"
+              />
+            </Carousel.Item>
+          </Carousel>
+        </div>
+        <div>
+          <Card style={{ width: "100%", height: "100%" }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+      <GoogleMap />
+
+      <Footer />
     </div>
   );
 };
