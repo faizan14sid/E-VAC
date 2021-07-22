add0cabd2e540a6599ce4c882321295f2034f50d
Neha Sontakke <wasnik.neha@gmail.com>
2021-07-22 16:28:04 +0530

    added images folder

--- e-vac/src/Components/Login.js
+++ e-vac/src/Components/Login.js
@@ -21,7 +21,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+        E-VAC
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
