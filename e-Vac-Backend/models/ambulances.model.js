932947dc0f062bed5802cb43e1a7c63f6b865388
faizan <faizan14sid@gmail.com>
2021-07-31 00:11:37 +0530

    backend include

---
+++ e-Vac-Backend/models/ambulances.model.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+import validator from 'validator';
+
+const AmbulanceSchema = new mongoose.Schema({
+    ambulanceNumber: {type: String , required: true},
+    hospitalName: {type: String,required: true},
+    price: {type: String,required: true},
+    productImage:{type:String,required:true},
+    reviewRating: {type: Number,required: true},
+    reviewCount:{type:Number,required:true},
+    distance:{type:Number,required:true},
+    online:{type:Boolean,required:true,default:true},
+    available:{type:Boolean,required:true,default:true}
+},
+);
+
+var AmbulanceModel = mongoose.model('AmbulanceModel', AmbulanceSchema);
+
+export default AmbulanceModel;
+
+// const orderSchema = mongoose.Schema({
+//     _id: mongoose.Schema.Types.ObjectId,
+//     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+//     order: [
+//         {
+//             product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+//             price: { type: Number, required: true},
+//             quantity: Number
+//         }
+//     ],
+//     address: { type: mongoose.Schema.Types.ObjectId, ref: 'UserAddress' },
+//     orderDate: { type: Date, default: Date.now() },
+//     paymentType: String,
+//     paymentStatus: String,
+//     isOrderCompleted: { type: Boolean, default: false }
+// });
