49776cf52c8e3d2170807689a5ebcf2c39907219
faizan <faizan14sid@gmail.com>
2021-08-03 13:23:30 +0530

    sending location to backend

--- e-vac-frontend/src/Components/Hospitals/Nearby.js
+++ e-vac-frontend/src/Components/Hospitals/Nearby.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Help from "../Help";
+
 
 const Nearby = () => {
   return (
     <div>
       <h1>Nearby hospitals</h1>
-      <Help />
+      
     </div>
   );
 };
