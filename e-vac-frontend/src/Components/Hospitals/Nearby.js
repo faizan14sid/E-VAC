532d43af98604663886d5e5c5662aa73addbe420
faizan <faizan14sid@gmail.com>
2021-08-04 21:15:28 +0530

    pull from backend opt

--- e-vac-frontend/src/Components/Hospitals/Nearby.js
+++ e-vac-frontend/src/Components/Hospitals/Nearby.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-
-const Nearby = () => {
-  return (
-    <div>
-      <h1>Nearby hospitals</h1>
-    </div>
-  );
-};
-
-export default Nearby;
-=======
 import React from "react";
 
 
@@ -20,10 +7,9 @@ const Nearby = () => {
   return (
     <div>
       <h1>Nearby hospitals</h1>
-    
+
     </div>
   );
 };
 
 export default Nearby;
->>>>>>> 4c3fbc63ffe8d7ede472b18f164781689fde37a7
