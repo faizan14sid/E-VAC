b6bb9c12b8df75151f831edb6a94399a5b1da39c
faizan <faizan14sid@gmail.com>
2021-08-04 21:17:48 +0530

    accept incoming changes

--- e-Vac-Backend/controllers/user.controller.js
+++ e-Vac-Backend/controllers/user.controller.js
@@ -11,13 +11,13 @@ import fast2sms from 'fast-two-sms';
 // pass your msg91 otp creditials SendOtp
 var Otp = 1234;
 // send otp for sending otp to entered phone number and also pass message sender name like app name from your credintials
-export const SENDOTP = async(req,res) => {
-    const OTP =  await Math.floor(1000 + Math.random() * 9000)   
-    Otp = OTP 
-    console.log(OTP, req.body)       
-    const response = await fast2sms.sendMessage({authorization:process.env.API_KEY, message:`${OTP} otp for E-Vac`, numbers:[req.body.phoneNumber]})
+export const SENDOTP = async (req, res) => {
+    const OTP = await Math.floor(1000 + Math.random() * 9000)
+    Otp = OTP
+    console.log(OTP, req.body)
+    const response = await fast2sms.sendMessage({ authorization: process.env.API_KEY, message: `${OTP} otp for E-Vac`, numbers: [req.body.phoneNumber] })
     res.send(response)
-    
+
 
     // sendOtp.send(req.body.phoneNumber, "Parmeshwar", (err, data) => {
     //     console.log(req.body.phoneNumber)
@@ -29,53 +29,53 @@ export const SENDOTP = async(req,res) => {
 }
 
 // verify otp to verify entered otp matched with sentotp or not
-export const VERIFYOTP = (req,res) => {
-    console.log(req.body.otp,Otp)
-    if(req.body.otp == Otp ){
+export const VERIFYOTP = (req, res) => {
+    console.log(req.body.otp, Otp)
+    if (req.body.otp == Otp) {
         // if (data.type == "success") {
-            let { phoneNumber } = req.body;
-            UserModel.findOne({ phoneNumber }, (err, user) => {
-                if (err) return  res.json({ err });
-                if (!user) {
-                    // user signup
-                    UserModel.create(req.body, (err, user) => {
-                        if (err) return  res.json({ err });
-                        jwt.sign(
-                            {
-                                userId:  user._id,
-                                phoneNumber:  user.phoneNumber
-                            },
-                            "thisissecret",
-                            (err, signuptoken) => {
-                                if (err) return  res.json({ err });
-                                res.json({
-                                    success:  true,
-                                    signuptoken,
-                                    userId:  user._id,
-                                    message:  "registered successfully"
-                                });
-                            }
-                        );
-                    });
-                }
-                if (user) {
-                    // user signin
+        let { phoneNumber } = req.body;
+        UserModel.findOne({ phoneNumber }, (err, user) => {
+            if (err) return res.json({ err });
+            if (!user) {
+                // user signup
+                UserModel.create(req.body, (err, user) => {
+                    if (err) return res.json({ err });
                     jwt.sign(
                         {
-                            userId:  user._id,
-                            phoneNumber:  user.phoneNumber
+                            userId: user._id,
+                            phoneNumber: user.phoneNumber
                         },
                         "thisissecret",
-                        (err, logintoken) => {
-                            if (err) return  res.json({ err });
-                            res.json({ logintoken, userId:  user._id });
+                        (err, signuptoken) => {
+                            if (err) return res.json({ err });
+                            res.json({
+                                success: true,
+                                signuptoken,
+                                userId: user._id,
+                                message: "registered successfully"
+                            });
                         }
                     );
-                }
-            });
+                });
+            }
+            if (user) {
+                // user signin
+                jwt.sign(
+                    {
+                        userId: user._id,
+                        phoneNumber: user.phoneNumber
+                    },
+                    "thisissecret",
+                    (err, logintoken) => {
+                        if (err) return res.json({ err });
+                        res.json({ logintoken, userId: user._id });
+                    }
+                );
+            }
+        });
         // }
         // if (data.type == "error") res.json({ success:  false, message:  data.message });
-    }else{
-        res.send({ message: "Wrong otp"})
+    } else {
+        res.send({ message: "Wrong otp" })
     };
 }
