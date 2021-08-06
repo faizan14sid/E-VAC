4350d25e1c68e67d4a5225edc55429e86dce9b1e
faizan <faizan14sid@gmail.com>
2021-08-06 18:25:31 +0530

    final

--- e-vac-frontend/src/Components/Login.js
+++ e-vac-frontend/src/Components/Login.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import logo from "../logo.png";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
-
 import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
